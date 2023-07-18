05abf13a52d88180b2282926c56754fe818e77bc
Avi Kaul <avikarkaul@gmail.com>
2023-07-17 23:22:31 -0400

    py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classes.project import Project
+from classes.manager import Manager
 
 def main():
     pass
@@ -16,6 +16,7 @@ def main():
 # print(avi)
 # project_flatiron = Project("Project Flatiron", "A highly secretive intelligence operation surveilling the productivity of developers in the NYC Metropolitan Area", "May 22, 2023", 1, 1)
 # print(project_flatiron)
+
 
     
     
